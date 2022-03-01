5870b3e4aa832b590cf90fc0abedc49a6373a5c5
Roberto Lorenzo Aguilar Maldonado <48602725+RobertoLorenzoAguilar@users.noreply.github.com>
2022-03-01 16:54:37 -0700

    Update covid_script.sh
    update script add a review file

--- covid_script.sh
+++ covid_script.sh
@@ -1,13 +1,14 @@
 if [[ ! -f datos_abiertos_covid19.zip ]]
 then
-    	echo "datos abiertos no existe"  
+    echo "datos abiertos no existe"  
 	echo "descargando datos abiertos covid19"
 	curl -O http://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip
 	echo "descomprimiendo datos abiertos covid19"
 	unzip  datos_abiertos_covid19.zip	
 	echo "datos covid encontrados, trabajando con el .csv"
-	csvcut  -c EMBARAZO,ENTIDAD_UM,SEXO,FECHA_DEF,EDAD,CLASIFICACION_FINAL  *COVID19MEXICO.csv  | csvgrep -c EMBARAZO -m "1"|  csvgrep -c ENTIDAD_UM -m "26"| csvgrep -c SEXO -m "1"| csvgrep -c CLASIFICACION_FINAL -m "1" > covid_size_reduction.csv	find * -type f -name '*.zip' -o -name '*.txt' | xargs rm;	
+	csvcut  -c EMBARAZO,ENTIDAD_UM,SEXO,FECHA_DEF,EDAD,CLASIFICACION_FINAL  *COVID19MEXICO.csv  | csvgrep -c EMBARAZO -m "1"|  csvgrep -c ENTIDAD_UM -m "26"| csvgrep -c SEXO -m "1"| csvgrep -c CLASIFICACION_FINAL -m "1" > covid_size_reduction.csv;	
 	echo "finanlizando datos covid"	
+	head -n 10 covid_size_reduction.csv > review_file.txt
+	cat review_file.txt
 fi
-
 
